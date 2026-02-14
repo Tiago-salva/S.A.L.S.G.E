2fb2f73a1cbdd5794a51324f02ae9fcaadfddf8d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-02-14 19:38:42 -0300

    feat: initial commit

---
+++ src/test/java/com/salsge/demo/SalsgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salsge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
