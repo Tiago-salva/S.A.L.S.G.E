254d68ef3d6d72967d6b49ba04b46a58a5393a08
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-02-16 09:13:10 -0300

    feat: implement simple CRUD of employees with console

--- src/main/java/com/salsge/demo/Employees/EmployeeRepository.java
+++ src/main/java/com/salsge/demo/Employees/EmployeeRepository.java
@@ -1,4 +1,6 @@
 package com.salsge.demo.Employees;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
