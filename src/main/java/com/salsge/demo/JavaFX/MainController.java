2422b14b1d533e37bff67dc45a285263264aee56
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-02-18 09:00:20 -0300

    feat: add functions to load each view of the options

---
+++ src/main/java/com/salsge/demo/JavaFX/MainController.java
@@ -0,0 +1,41 @@
+package com.salsge.demo.JavaFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+
+    public void loadEmployeesView() throws IOException {
+        openFxmlView("/views/Employees.fxml", "Employees");
+    }
+
+    public void loadPayrollView() throws IOException {
+        // openFxmlView("/views/Home.fxml", "Employees");
+    }
+    public void loadLegajosView() throws IOException {
+        // openFxmlView("/views/Home.fxml", "Employees");
+    }
+    public void loadNovedadesView() throws IOException {
+        // openFxmlView("/views/Home.fxml", "Employees");
+    }
+
+    public void openFxmlView(String path, String title) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader((getClass().getResource(path)));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 500, 500);
+
+        Stage stage = new Stage();
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
