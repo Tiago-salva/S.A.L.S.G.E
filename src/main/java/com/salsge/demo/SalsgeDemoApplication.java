1247d03b0cbf0f336900b3f61b4b4e0ebc563df7
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-02-18 08:58:37 -0300

    feat: add JavaFX to replace the use of the console

--- src/main/java/com/salsge/demo/SalsgeDemoApplication.java
+++ src/main/java/com/salsge/demo/SalsgeDemoApplication.java
@@ -1,50 +1,44 @@
 package com.salsge.demo;
 
+import com.salsge.demo.JavaFX.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
 
 @SpringBootApplication
-public class SalsgeDemoApplication {
+public class SalsgeDemoApplication extends Application {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SalsgeDemoApplication.class, args);
 
-		boolean salir = false;
-		Scanner sn = new Scanner(System.in);
-
-		while(!salir) {
-
-			System.out.println("------ MENU ------");
-			System.out.println("""
-					1. LISTAR TODOS LOS EMPLEADOS
-					2. LISTAR EMPLEADO
-					3. CREAR EMPLEADO
-					4. EDITAR EMPLEADO
-					5. ELIMINAR EMPLEADO
-					6. SALIR
-				""");
-
-
-			System.out.print("Elige una opcion: ");
-			int opcionNumeroUsuario = sn.nextInt();
-
-			if(opcionNumeroUsuario <= 0 || opcionNumeroUsuario > 6) {
-				throw new IllegalArgumentException("Opcion invalida");
-			}
-
-			ConsoleOption opcionUsuario = ConsoleOption.convertEnum(opcionNumeroUsuario);
-
-			salir = opcionUsuario.execute();
-
-		}
-
-		sn.close();
+		launch(args);
 
 	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		try {
 
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Home.fxml"));
+			Parent root = loader.load();
+
+			MainController mainController = loader.getController();
+
+			Scene scene = new Scene(root);
+			stage.setTitle("S.A.L.S.G.E");
+			stage.setScene(scene);
+			stage.show();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
