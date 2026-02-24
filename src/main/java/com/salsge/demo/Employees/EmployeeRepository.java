fe1b6a8d58c171b7ad578ff62bdd074c543a7809
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-02-24 20:09:55 -0300

    feat: implement personalized function to retrieve employees by fullName

--- src/main/java/com/salsge/demo/Employees/EmployeeRepository.java
+++ src/main/java/com/salsge/demo/Employees/EmployeeRepository.java
@@ -1,6 +1,13 @@
 package com.salsge.demo.Employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByFullNameContainingIgnoreCase(String fullName);
+
 }
