f33aeff3990f82b9a1e1d1eab7e78a549a32267f
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-02-24 20:15:34 -0300

    feat: add configuration to enable right connection between Spring and JavaFX

--- src/main/java/com/salsge/demo/SalsgeDemoApplication.java
+++ src/main/java/com/salsge/demo/SalsgeDemoApplication.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Scanner;
 
@@ -22,23 +23,28 @@ public class SalsgeDemoApplication extends Application {
 
 	}
 
+	private ConfigurableApplicationContext applicationContext;
+
+	@Override
+	public void init() {
+		applicationContext = SpringApplication.run(SalsgeDemoApplication.class);
+	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
-		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Employees.fxml"));
 
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Home.fxml"));
-			Parent root = loader.load();
+		loader.setControllerFactory(applicationContext::getBean);
 
-			MainController mainController = loader.getController();
+		Parent root = loader.load();
+		stage.setScene(new Scene(root));
+		stage.setTitle("S.A.L.S.G.E");
+		stage.show();
+	}
 
-			Scene scene = new Scene(root);
-			stage.setTitle("S.A.L.S.G.E");
-			stage.setScene(scene);
-			stage.show();
-
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+	@Override
+	public void stop() {
+		applicationContext.close();
 	}
 
 }
