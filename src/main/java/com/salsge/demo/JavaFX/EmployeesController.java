8ed22237f07d88cebbf4e87686f504969f87e471
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-02-25 19:53:34 -0300

    feat: add functionality to show legajo form when click on employee column

--- src/main/java/com/salsge/demo/JavaFX/EmployeesController.java
+++ src/main/java/com/salsge/demo/JavaFX/EmployeesController.java
@@ -5,12 +5,17 @@ import com.salsge.demo.Employees.EmployeeService;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class EmployeesController {
@@ -21,14 +26,27 @@ public class EmployeesController {
     @FXML private TableView<Employee> allEmployeesView;
     @FXML private TableColumn<Employee, String> colFullName;
     @FXML private TextField employeeName;
+    @FXML private StackPane legajoContainer;
+
+    private void loadLegajoView(Employee employee) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Legajo.fxml"));
+            Parent view = loader.load();
+
+            LegajoController controller = loader.getController();
+            controller.setEmployee(employee);
+
+            legajoContainer.getChildren().setAll(view);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     public void renderAllEmployees(String employeeFullName) {
-
         colFullName.setCellValueFactory(new PropertyValueFactory<>("fullName"));
-
         allEmployeesView.setItems(FXCollections.observableArrayList(employeeService.getEmployeeByName(employeeFullName)));
-
     }
 
     @FXML
@@ -37,6 +55,14 @@ public class EmployeesController {
             System.out.println(newValue);
             renderAllEmployees(newValue);
         });
+
+        allEmployeesView.getSelectionModel().selectedItemProperty()
+                .addListener((obs, oldEmp, newEmp) -> {
+                    if (newEmp != null) {
+                        loadLegajoView(newEmp);
+                    }
+                });
+
     }
 
 }
