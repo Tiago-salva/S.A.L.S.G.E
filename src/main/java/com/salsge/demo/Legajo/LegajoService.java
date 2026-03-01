0e39e3c294de03de0e632a5a4d45d33ea7bbab0f
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-01 19:29:40 -0300

    feat: add function to edit legajos

--- src/main/java/com/salsge/demo/Legajo/LegajoService.java
+++ src/main/java/com/salsge/demo/Legajo/LegajoService.java
@@ -32,5 +32,36 @@ public class LegajoService {
         legajoRepository.save(legajoData);
     }
 
-    // Agregar editLegajo en un futuro
+    public void editLegajo(Long legajoId, Legajo legajoData) {
+        Legajo legajo = getLegajo(legajoId).orElseThrow(() -> new RuntimeException("Legajo not found"));
+
+        legajo.setNumeroDeLegajo(legajoData.getNumeroDeLegajo());
+        legajo.setLastNames(legajoData.getLastNames());
+        legajo.setNames(legajoData.getNames());
+        legajo.setDireccion(legajoData.getDireccion());
+        legajo.setNumeroDeDireccion(legajoData.getNumeroDeDireccion());
+        legajo.setPiso(legajoData.getPiso());
+        legajo.setDepto(legajoData.getDepto());
+        legajo.setCodigoPostal(legajoData.getCodigoPostal());
+        legajo.setLocalidad(legajoData.getLocalidad());
+        legajo.setDni(legajoData.getDni());
+        legajo.setCuit(legajoData.getCuit());
+        legajo.setTelefono(legajoData.getTelefono());
+        legajo.setTelefonoDeEmergencia(legajoData.getTelefonoDeEmergencia());
+        legajo.setEmail(legajoData.getEmail());
+        legajo.setFechaDeNacimiento(legajoData.getFechaDeNacimiento());
+        legajo.setFechaDeIngreso(legajoData.getFechaDeIngreso());
+        legajo.setSexo(legajoData.getSexo());
+        legajo.setEstadoCivil(legajoData.getEstadoCivil());
+        legajo.setCbu(legajoData.getCbu());
+        legajo.setCta(legajoData.getCta());
+        legajo.setBanco(legajoData.getBanco());
+        legajo.setSueldo(legajoData.getSueldo());
+        legajo.setTipoEmpleado(legajoData.getTipoEmpleado());
+        legajo.setConvenio(legajoData.getConvenio());
+        legajo.setObraSocial(legajoData.getObraSocial());
+
+        legajoRepository.save(legajo);
+
+    }
 }
