e3295dce1c9745efd37c0d5910e539acb52f3b50
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-04 10:37:26 -0300

    feat: add function to retrieve employee by his legajo number

--- src/main/java/com/salsge/demo/Employees/EmployeeRepository.java
+++ src/main/java/com/salsge/demo/Employees/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
@@ -11,5 +12,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findByFullNameContainingIgnoreCase(String fullName);
 
     List<Employee> findDistinctByLegajoNumeroDeLegajoContaining(String numeroDeLegajo);
+
+    Optional<Employee> findByLegajoNumeroDeLegajoIgnoreCase(String numeroDeLegajo);
 
 }
