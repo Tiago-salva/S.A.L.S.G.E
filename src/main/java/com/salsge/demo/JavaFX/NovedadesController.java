ad720edca060a7e1e86081b3c3eb321714da9378
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-07 10:33:52 -0300

    feat: add functionality to show employee with the same legajoNumber when enter is pressed

--- src/main/java/com/salsge/demo/JavaFX/NovedadesController.java
+++ src/main/java/com/salsge/demo/JavaFX/NovedadesController.java
@@ -2,13 +2,20 @@ package com.salsge.demo.JavaFX;
 
 import com.salsge.demo.Employees.Employee;
 import com.salsge.demo.Employees.EmployeeService;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
-public class NovedadesController {
+public class NovedadesController implements Initializable {
 
     @Autowired
     EmployeeService employeeService;
@@ -31,5 +38,17 @@ public class NovedadesController {
         Employee employee = employeeService.getEmployeeByLegajo(legajoNumber).orElseThrow(() -> new RuntimeException("No existe empleado con ese legajo"));
 
         novColaboradorField.setText(employee.getFullName());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        novLegajoField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent ke) {
+                if (ke.getCode().equals(KeyCode.ENTER)) {
+                    renderEmployeeByLegajo();
+                }
+            }
+        });
     }
 }
