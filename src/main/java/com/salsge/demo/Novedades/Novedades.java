8cad95f653398ce069691bfaa8e6037e66fe668e
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-11 20:11:23 -0300

    feat: add columns to novedades table

--- src/main/java/com/salsge/demo/Novedades/Novedades.java
+++ src/main/java/com/salsge/demo/Novedades/Novedades.java
@@ -1,4 +1,40 @@
 package com.salsge.demo.Novedades;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public class Novedades {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Positive
+    @Column(nullable = false)
+    Integer legajoNumber;
+
+    @NotBlank
+    @Column(nullable = false)
+    String colaborador;
+
+    @NotBlank
+    @Column(nullable = false)
+    String concepto;
+
+    @Positive
+    @Column(nullable = false)
+    Integer dias;
+
+    @Positive
+    @Column(nullable = false)
+    Integer horas;
+
+    @Positive
+    @Column(nullable = false)
+    Integer importe;
+
 }
