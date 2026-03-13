b3e59a241bfd5cfab35ba177f7369a850e3ba013
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-12 21:23:33 -0300

    feat: add function to retrieve legajo by legajoNumber

--- src/main/java/com/salsge/demo/Legajo/LegajoRepository.java
+++ src/main/java/com/salsge/demo/Legajo/LegajoRepository.java
@@ -2,5 +2,10 @@ package com.salsge.demo.Legajo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LegajoRepository extends JpaRepository<Legajo, Long> {
+
+    Optional<Legajo> findByNumeroDeLegajoContainingIgnoreCase(Integer legajoNumber);
+
 }
