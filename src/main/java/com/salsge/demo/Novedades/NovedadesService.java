abc21287302cf285a711620d0ba79bde3ef2f443
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-12 21:26:48 -0300

    feat: add createNovedades function

--- src/main/java/com/salsge/demo/Novedades/NovedadesService.java
+++ src/main/java/com/salsge/demo/Novedades/NovedadesService.java
@@ -1,4 +1,29 @@
 package com.salsge.demo.Novedades;
 
+import com.salsge.demo.Legajo.Legajo;
+import com.salsge.demo.Legajo.LegajoRepository;
+import com.salsge.demo.Legajo.LegajoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Service
 public class NovedadesService {
+
+    @Autowired
+    NovedadesRepository novedadesRepository;
+
+    @Autowired
+    LegajoService legajoService;
+
+    public void createNovedades(Novedades novedades) {
+        novedadesRepository.save(novedades);
+
+        Legajo legajo = legajoService.getLegajoByNumber(novedades.getLegajoNumber()).orElseThrow(() -> new RuntimeException("No existe ningun legajo con ese numero"));
+
+        legajo.setNovedades(novedades);
+
+    }
+
 }
