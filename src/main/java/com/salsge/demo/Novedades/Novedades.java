89452cf05dd199355805bc6d484dee3976be49c2
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-12 21:24:49 -0300

    feat: add constructor, getters and setters for novedades class

--- src/main/java/com/salsge/demo/Novedades/Novedades.java
+++ src/main/java/com/salsge/demo/Novedades/Novedades.java
@@ -37,4 +37,73 @@ public class Novedades {
     @Column(nullable = false)
     Integer importe;
 
+    // Constructores
+    public Novedades() {}
+
+    public Novedades(Long id, Integer legajoNumber, String colaborador, String concepto, Integer dias, Integer horas, Integer importe) {
+        this.id = id;
+        this.legajoNumber = legajoNumber;
+        this.colaborador = colaborador;
+        this.concepto = concepto;
+        this.dias = dias;
+        this.horas = horas;
+        this.importe = importe;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @Positive Integer getLegajoNumber() {
+        return legajoNumber;
+    }
+
+    public void setLegajoNumber(@Positive Integer legajoNumber) {
+        this.legajoNumber = legajoNumber;
+    }
+
+    public @NotBlank String getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(@NotBlank String colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public @NotBlank String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(@NotBlank String concepto) {
+        this.concepto = concepto;
+    }
+
+    public @Positive Integer getDias() {
+        return dias;
+    }
+
+    public void setDias(@Positive Integer dias) {
+        this.dias = dias;
+    }
+
+    public @Positive Integer getHoras() {
+        return horas;
+    }
+
+    public void setHoras(@Positive Integer horas) {
+        this.horas = horas;
+    }
+
+    public @Positive Integer getImporte() {
+        return importe;
+    }
+
+    public void setImporte(@Positive Integer importe) {
+        this.importe = importe;
+    }
 }
