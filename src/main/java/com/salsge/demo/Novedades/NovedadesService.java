d88fdfb897c1a4f6a47275389b995b8d37f88fd8
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-14 18:14:51 -0300

    feat: now createNovedades receive the data and add the novedad to the db

--- src/main/java/com/salsge/demo/Novedades/NovedadesService.java
+++ src/main/java/com/salsge/demo/Novedades/NovedadesService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.List;
+
 @Validated
 @Service
 public class NovedadesService {
@@ -17,12 +19,22 @@ public class NovedadesService {
     @Autowired
     LegajoService legajoService;
 
-    public void createNovedades(Novedades novedades) {
+    public void createNovedades(String legajoNumber, String colaborador,
+                                String concepto, Integer horas,
+                                Integer dias, Integer importe) {
+
+        Legajo legajo = legajoService.getLegajoByNumber(legajoNumber).orElseThrow(() -> new RuntimeException("No existe ningun legajo con ese numero"));
+
+        Novedades novedades = new Novedades();
+
+        novedades.setLegajo(legajo);
+        novedades.setColaborador(colaborador);
+        novedades.setConcepto(concepto);
+        novedades.setHoras(horas);
+        novedades.setDias(dias);
+        novedades.setImporte(importe);
+
         novedadesRepository.save(novedades);
-
-        Legajo legajo = legajoService.getLegajoByNumber(novedades.getLegajoNumber()).orElseThrow(() -> new RuntimeException("No existe ningun legajo con ese numero"));
-
-        legajo.setNovedades(novedades);
 
     }
 
