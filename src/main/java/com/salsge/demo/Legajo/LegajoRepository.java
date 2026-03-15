672113cc8a0b38a99f38ee063e78b488cd974c52
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-15 12:17:17 -0300

    feat: add function to retrieve a list of legajos by legajo numbers

--- src/main/java/com/salsge/demo/Legajo/LegajoRepository.java
+++ src/main/java/com/salsge/demo/Legajo/LegajoRepository.java
@@ -2,10 +2,13 @@ package com.salsge.demo.Legajo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LegajoRepository extends JpaRepository<Legajo, Long> {
 
     Optional<Legajo> findByNumeroDeLegajoContainingIgnoreCase(String legajoNumber);
+
+    List<Legajo> findByNumeroDeLegajoIn(List<String> legajoNumbers);
 
 }
