3612dc4a624ca5685784e5fa8b53efc05ce01645
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-16 10:23:23 -0300

    feat: implement functionality to overwrite novedades if the legajo and concepto are the same

--- src/main/java/com/salsge/demo/Novedades/NovedadService.java
+++ src/main/java/com/salsge/demo/Novedades/NovedadService.java
@@ -1,11 +1,15 @@
 package com.salsge.demo.Novedades;
 
 import com.salsge.demo.Conceptos.Concepto;
+import com.salsge.demo.Conceptos.ConceptoService;
 import com.salsge.demo.Legajo.Legajo;
 import com.salsge.demo.Legajo.LegajoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Optional;
 
 @Validated
 @Service
@@ -17,13 +21,33 @@ public class NovedadService {
     @Autowired
     LegajoService legajoService;
 
+    @Autowired
+    ConceptoService conceptoService;
+
     public void createNovedad(Novedad novedad) {
 
-        // Esta linea no tendria que servir para nada
-        // Ya que al pegar en la TableView checkea si existe algun legajo con el numero pegado
-        Legajo legajo = legajoService.getLegajoByNumber(novedad.getLegajo().getNumeroDeLegajo()).orElseThrow(() -> new RuntimeException("No existe ningun legajo con ese numero"));
+        // Legajo legajo = legajoService.getLegajoByNumber(novedad.getLegajo().getNumeroDeLegajo()).orElseThrow(() -> new RuntimeException("No existe ningun legajo con ese numero"));
 
-        novedadRepository.save(novedad);
+        // Checkear si el legajo ya tiene una novedad con ese mismo concepto
+        Legajo legajo = novedad.getLegajo();
+        Concepto concepto = novedad.getConcepto();
+
+        // Obtener novedad con ese mismo legajo y concepto
+        Optional<Novedad> existeNovedad = novedadRepository.findByLegajoAndConcepto(legajo, concepto);
+
+        // Si existe, sobreescribir
+        if(existeNovedad.isPresent()) {
+            Novedad n = existeNovedad.get();
+            n.setConcepto(novedad.getConcepto());
+            n.setHoras(novedad.getHoras());
+            n.setDias(novedad.getDias());
+            n.setImporte(novedad.getImporte());
+            novedadRepository.save(n);
+        }
+        // Sino, crearlo
+        else {
+            novedadRepository.save(novedad);
+        }
 
     }
 
