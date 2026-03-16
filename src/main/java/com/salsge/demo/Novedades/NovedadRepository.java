aea3e40f44a510b96df9c6219a986cd162864bfd
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-16 10:22:03 -0300

    feat: add function to retrieve a novedad by legajo and concepto

--- src/main/java/com/salsge/demo/Novedades/NovedadRepository.java
+++ src/main/java/com/salsge/demo/Novedades/NovedadRepository.java
@@ -1,8 +1,15 @@
 package com.salsge.demo.Novedades;
 
+import com.salsge.demo.Conceptos.Concepto;
+import com.salsge.demo.Legajo.Legajo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NovedadRepository extends JpaRepository<Novedad, Long> {
+
+    Optional<Novedad> findByLegajoAndConcepto(Legajo legajo, Concepto concepto);
+
 }
