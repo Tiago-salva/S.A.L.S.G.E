b03faa2c32aa9fb308938265440dca570f5d51f0
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-26 21:17:09 -0300

    feat: add categoria class and his getters, setters and constructor

---
+++ src/main/java/com/salsge/demo/Categorias/Categoria.java
@@ -0,0 +1,50 @@
+package com.salsge.demo.Categorias;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String categoriaName;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String sueldo;
+
+    public Categoria(Long id, String categoriaName, String sueldo) {
+        this.id = id;
+        this.categoriaName = categoriaName;
+        this.sueldo = sueldo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoriaName() {
+        return categoriaName;
+    }
+
+    public void setCategoriaName(String categoriaName) {
+        this.categoriaName = categoriaName;
+    }
+
+    public String getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(String sueldo) {
+        this.sueldo = sueldo;
+    }
+}
