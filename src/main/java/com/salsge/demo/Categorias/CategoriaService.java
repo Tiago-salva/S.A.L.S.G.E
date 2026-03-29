0672cc45b49232974d8540f35cc91c2cb2f5f23f
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-29 20:46:51 -0300

    feat: add constructor with categoriaRepository

--- src/main/java/com/salsge/demo/Categorias/CategoriaService.java
+++ src/main/java/com/salsge/demo/Categorias/CategoriaService.java
@@ -3,6 +3,7 @@ package com.salsge.demo.Categorias;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,10 @@ public class CategoriaService {
 
     @Autowired
     CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
 
     public List<Categoria> getAllCategorias() {
         return categoriaRepository.findAll();
