60d249ef477458fe6b249f4aa4e5be2d10eea307
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-29 20:46:27 -0300

    feat: add toString method

--- src/main/java/com/salsge/demo/Categorias/Categoria.java
+++ src/main/java/com/salsge/demo/Categorias/Categoria.java
@@ -50,4 +50,9 @@ public class Categoria {
     public void setSueldo(String sueldo) {
         this.sueldo = sueldo;
     }
+
+    @Override
+    public String toString() {
+        return this.categoriaName;
+    }
 }
