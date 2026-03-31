6fc5c7268df1fc0beb3f6162417af06ed60f8a05
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-03-31 10:32:53 -0300

    feat: add @Repository annotation

--- src/main/java/com/salsge/demo/Liquidacion/LiquidacionRepository.java
+++ src/main/java/com/salsge/demo/Liquidacion/LiquidacionRepository.java
@@ -1,6 +1,8 @@
 package com.salsge.demo.Liquidacion;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LiquidacionRepository extends JpaRepository<Liquidacion, Long> {
 }
