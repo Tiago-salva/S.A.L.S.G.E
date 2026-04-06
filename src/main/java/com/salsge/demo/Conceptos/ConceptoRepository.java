9a60b23212bbaeba1e1cd7a9b5a0c6c770f2b9f3
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-04-06 11:09:03 -0300

    feat: add function to retrieve conceptos from an array

--- src/main/java/com/salsge/demo/Conceptos/ConceptoRepository.java
+++ src/main/java/com/salsge/demo/Conceptos/ConceptoRepository.java
@@ -3,11 +3,15 @@ package com.salsge.demo.Conceptos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConceptoRepository extends JpaRepository<Concepto, Long> {
 
     boolean existsByConceptoNameOrCodigoConcepto(String conceptoName, String codigoConcepto);
 
     void deleteByConceptoName(String conceptoName);
+
+    List<Concepto> findByCodigoConceptoIn(List<String> codigos);
 
 }
