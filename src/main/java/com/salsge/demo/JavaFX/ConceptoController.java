05ac1725c5d7e08550cecbbef97df14c6875b725
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2026-04-07 09:31:14 -0300

    feat: add editConcepto and getSelectedConcepto functions

--- src/main/java/com/salsge/demo/JavaFX/ConceptoController.java
+++ src/main/java/com/salsge/demo/JavaFX/ConceptoController.java
@@ -25,12 +25,13 @@ public class ConceptoController implements Initializable {
     @FXML private TableColumn<Concepto, String> conceptoCodigoCol;
     @FXML private TableColumn<Concepto, String> conceptoNameCol;
     @FXML private TableColumn<Concepto, String> conceptoTypeCol;
+    @FXML private TableColumn<Concepto, String> conceptoFormulaCol;
 
     // Create conceptos
     @FXML private TextField conceptoNameField;
     @FXML private TextField conceptoCodigoField;
     @FXML private TextField conceptoTipoField;
-    @FXML private Button conceptoButton;
+    @FXML private TextField conceptoFormulaField;
     @FXML private Text notificationText;
 
     @Override
@@ -38,8 +39,20 @@ public class ConceptoController implements Initializable {
         conceptoCodigoCol.setCellValueFactory(new PropertyValueFactory<>("codigoConcepto"));
         conceptoNameCol.setCellValueFactory(new PropertyValueFactory<>("conceptoName"));
         conceptoTypeCol.setCellValueFactory(new PropertyValueFactory<>("tipoDeConcepto"));
+        conceptoFormulaCol.setCellValueFactory(new PropertyValueFactory<>("formula"));
 
         conceptoView.setItems(FXCollections.observableArrayList(conceptoService.getAllConceptos()));
+
+        conceptoView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                conceptoNameField.setText(newValue.getConceptoName());
+                conceptoCodigoField.setText(newValue.getCodigoConcepto());
+                conceptoTipoField.setText(newValue.getTipoDeConcepto());
+                conceptoFormulaField.setText(newValue.getFormula());
+                notificationText.setText("");
+            }
+        });
+
     }
 
     @FXML
@@ -50,8 +63,9 @@ public class ConceptoController implements Initializable {
         String conceptoName = conceptoNameField.getText();
         String conceptoCodigo = conceptoCodigoField.getText();
         String conceptoTipo = conceptoTipoField.getText();
+        String conceptoFormula = conceptoFormulaField.getText();
 
-        if(conceptoName.isEmpty() || conceptoCodigo.isEmpty() || conceptoTipo.isEmpty()) {
+        if(conceptoName.isEmpty() || conceptoCodigo.isEmpty() || conceptoTipo.isEmpty() || conceptoFormula.isEmpty()) {
             notificationText.setText("No puede haber campos vacios");
             return;
         }
@@ -59,6 +73,7 @@ public class ConceptoController implements Initializable {
         concepto.setConceptoName(conceptoName);
         concepto.setCodigoConcepto(conceptoCodigo);
         concepto.setTipoDeConcepto(conceptoTipo);
+        concepto.setFormula(conceptoFormula);
 
         try {
             conceptoService.createConcepto(concepto);
@@ -70,18 +85,42 @@ public class ConceptoController implements Initializable {
 
     }
 
-    @FXML
-    private void deleteConcepto() {
+    private Concepto getSelectedConcepto() {
         Concepto conceptoSelected = conceptoView.getSelectionModel().getSelectedItem();
         if(conceptoSelected == null) {
             notificationText.setText("There's not concepto selected");
-        } else {
-            String conceptoSelectedName = conceptoSelected.getConceptoName();
-
-            conceptoService.deleteConcepto(conceptoSelectedName);
-            conceptoView.setItems(FXCollections.observableArrayList(conceptoService.getAllConceptos()));
+            return null;
         }
 
+        return conceptoSelected;
+
+    }
+
+    @FXML
+    private void deleteConcepto() {
+        Concepto conceptoSelected = getSelectedConcepto();
+        String conceptoSelectedName = conceptoSelected.getConceptoName();
+
+        conceptoService.deleteConcepto(conceptoSelectedName);
+        conceptoView.setItems(FXCollections.observableArrayList(conceptoService.getAllConceptos()));
+
+    }
+
+    @FXML
+    private void editConcepto() {
+        Concepto conceptoSelected = getSelectedConcepto();
+
+        String conceptoName = conceptoNameField.getText();
+        String conceptoCodigo = conceptoCodigoField.getText();
+        String conceptoTipo = conceptoTipoField.getText();
+        String conceptoFormula = conceptoFormulaField.getText();
+
+        if(conceptoName.isEmpty() || conceptoCodigo.isEmpty() || conceptoTipo.isEmpty() || conceptoFormula.isEmpty()) {
+            notificationText.setText("No puede haber campos vacios");
+        } else {
+            conceptoService.editConcepto(conceptoSelected, conceptoName, conceptoCodigo, conceptoTipo, conceptoFormula);
+            conceptoView.setItems(FXCollections.observableArrayList(conceptoService.getAllConceptos()));
+        }
     }
 
 }
